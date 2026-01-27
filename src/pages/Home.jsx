f97508a262a7ea6d26c891f263a92b243e9aa0b5
sahilsarmalkar <sahilgaming667@gmail.com>
2026-01-27 23:48:18 +0530

    added routing and updated gsap

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="bg-zinc-800 w-full h-screen"></div>
+      <div className="bg-white w-full h-screen"></div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Schedule.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Schedule = () => {
+  return (
+    <div><div className="bg-zinc-800 w-full h-screen"></div>
+      <div className="bg-white w-full h-screen"></div></div>
+  )
+}
