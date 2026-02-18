be0fb4ae349fbf1625ef5277b67c765aa2a56519
sahilsarmalkar <sahilgaming667@gmail.com>
2026-02-18 17:47:57 +0530

    modified all the things-1

--- src/hook/useSmoothScroll.js
+++ src/hook/useSmoothScroll.js
@@ -1,5 +1,3 @@
-// hooks/useSmoothScroll.js
-
 import { useEffect } from "react";
 import Lenis from "@studio-freight/lenis";
 import gsap from "gsap";
@@ -12,6 +10,7 @@ export const useSmoothScroll = () => {
     const lenis = new Lenis({
       duration: 1.2,
       smooth: true,
+      smoothTouch: false,
     });
 
     function raf(time) {
@@ -21,13 +20,30 @@ export const useSmoothScroll = () => {
 
     requestAnimationFrame(raf);
 
+    // Sync ScrollTrigger with Lenis
     lenis.on("scroll", ScrollTrigger.update);
 
-    ScrollTrigger.addEventListener("refresh", () => lenis.resize());
+    ScrollTrigger.scrollerProxy(document.body, {
+      scrollTop(value) {
+        return arguments.length
+          ? lenis.scrollTo(value)
+          : lenis.scroll.instance.scroll;
+      },
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+    });
+
     ScrollTrigger.refresh();
 
     return () => {
       lenis.destroy();
+      ScrollTrigger.getAll().forEach((t) => t.kill());
     };
   }, []);
 };
